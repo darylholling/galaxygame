974902316866bf5540124a2a4f428013cfd14e11
Verline <38378016+vlinpalm@users.noreply.github.com>
2020-06-14 14:52:30 +0200

    Test

--- src/Galaxy.java
+++ src/Galaxy.java
@@ -1,132 +1,151 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Galaxy extends Application {
     GridPane gp;
     int board_size = 12;
+    int planetScore = 0;
+    Location[][] location;
     @Override
     public void start(Stage stage) throws Exception {
         gp = new GridPane();
-//        gp.setGridLinesVisible(true);
         gp.setPrefSize(600, 600);
-
         LocationService locationService = new LocationService();
-        Location[][] gridPaneNodes = locationService.initalize(gp);
+        location = locationService.initalize(gp);
 
         // add spaceship
         Spaceship spaceship = new Spaceship();
-        spaceship.setLocation(gridPaneNodes[0][0]);
-        gp.add(spaceship,0,0);
-        gp.setColumnIndex(spaceship, spaceship.getLocation().getRight().getColumn());
-//        gp.setRowIndex(spaceship,  gp.getRowIndex(spaceship));
-        System.out.println(gp.getColumnIndex(spaceship));
+        spaceship.setLocation(location[0][0]);
+        gp.add(spaceship, spaceship.getLocation().getColumn(), spaceship.getLocation().getColumn());
 
 
-        //druk op D
+//        adds 4 planets at random locations
+        addObject(location, 4, "planet");
 
-//        for (int i = 0; i < board_size; i++) {
-//            for (int j = 0; j < board_size; j++) {
-////                gridPaneNodes[i][j] = new Location(i, j);
-//                Rectangle tile = new Rectangle(50, 50);
-//                tile.setFill(Color.TRANSPARENT);
-////                tile.setStroke(Color.BLACK);
-//                gp.add(tile,i,j);
-//
-//            }
-//        }
-
-//
-//        for (int i = 0; i < board_size; i++) {
-//            for (int j = 0; j < board_size; j++) {
-//                if (i + 1 < board_size) {
-//                    //setright
-//                    gp.setColumnIndex(spaceship, (gp.getColumnIndex(spaceship)+1));
-//                    gp.setRowIndex(spaceship,  gp.getRowIndex(spaceship));
-//                }
-//
-//                if (i - 1 < board_size && i - 1 > 0) {
-//                    gridPaneNodes[i][j].setLeft(gridPaneNodes[i - 1][j]);
-//                }
-//
-//                if (j + 1 < board_size) {
-//                    gridPaneNodes[i][j].setDown(gridPaneNodes[i][j + 1]);
-//                }
-//
-//                if (j - 1 < board_size && j - 1 > 0) {
-//                    gridPaneNodes[i][j].setUp(gridPaneNodes[i][j - 1]);
-//                }
-//            }
-//        }
-
-//        spaceship.setLocation(gridPaneNodes[spaceship.getLocation().getRight().getX()][spaceship.getLocation().getRight().getY()]);
-//        gp.getChildren().add(spaceship);
-//        spaceship.setLocation(spaceship.getLocation().getRight());
-
-//        System.out.println(gridPaneNodes[5][5].getRight().getX());
-//        System.out.println(gridPaneNodes[5][5].getRight().getY());
-//        Spaceship spaceship = new Spaceship();
-
-//        gp.add(spaceship, 0, 0);
-//        gridPaneNodes[0][0] = space;
-//
-////        adds 4 planets at random locations
-//        addObject(4, "planet");
-//
 //        //adds 5 meteorites at random locations
-//        addObject(5, "meteorite");
+        addObject(location,5, "meteorite");
 
         // if all planets visited , add wormhole
-        //addObject(1, "wormhole");
+        //addObject(location, 1, "wormhole");
 
         gp.setStyle("-fx-background-image: url('wp1.jpg');");
-
-// this function lists the content of our gridPaneNodes[][] for debugging purposes
-//        for (int i = 0; i < board_size; i++) {
-//            for (int j = 0; j < board_size; j++) {
-//                System.out.println(gridPaneNodes[i][j]);
-//            }
-//        }
 
         Scene scene = new Scene(gp, 600, 600);
         stage.setScene(scene);
         stage.show();
+        //move spaceship around with arrows
+        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent keyEvent) {
+                switch(keyEvent.getCode()){
+                    case UP: moveUp(spaceship, spaceship.getLocation().getUp().getColumn(),spaceship.getLocation().getUp().getRow()); break;
+                    case DOWN: moveDown(spaceship, spaceship.getLocation().getDown().getColumn(),spaceship.getLocation().getDown().getRow()); break;
+                    case LEFT: moveLeft(spaceship, spaceship.getLocation().getLeft().getColumn(),spaceship.getLocation().getLeft().getRow()); break;
+                    case RIGHT: moveRight(spaceship, spaceship.getLocation().getRight().getColumn(),spaceship.getLocation().getRight().getRow()); break;
+                }
+            }
+        });
     }
 
-    public void move(Sprite sprite, int posX, int posY, GridPane gridPane) {
-        //@todo add some scan/move function here to scan the surrounding grids and move using posX and posY +1 or -1
+    //move spaceship around with arrows
+
+    public void move(Sprite sprite, Location[][] location, int column, int row, int columnAfter, int RowAfter){
+        sprite.setLocation(location[column][row]);
+        gp.add(sprite, sprite.getLocation().getColumn(), sprite.getLocation().getColumn());
+    }
+    public void moveUp(Sprite sprite, int column , int row) {
+        int rowAfter = (row == 0 ? 0 : (row-1));
+        int columnAfter = column;
+        if (location[columnAfter][rowAfter].hasPlanet() && rowAfter >= 0){
+            visitPlanet(sprite, column, row);}
+        else if (location[columnAfter][rowAfter].hasMeteorite() || rowAfter < 0) {
+            System.out.println("DEAD"); //@todo: gameover
+        } else {
+            move(sprite, location, column, row, columnAfter, rowAfter);
+        }
+    }
+    public void moveDown(Sprite sprite, int column, int row) {
+        int rowAfter = (row == 11 ? 11 : (row+1));
+        int columnAfter = column;
+        if (location[columnAfter][rowAfter].hasPlanet() && rowAfter < 12){
+            visitPlanet(sprite, column, row);}
+        else if (location[columnAfter][rowAfter].hasMeteorite()|| (rowAfter > 11)) {
+            System.out.println("DEAD"); //@todo: gameover
+        } else {
+            move(sprite, location, column, row, columnAfter, rowAfter);
+        }
+    }
+    public void moveLeft(Sprite sprite, int column, int row) {
+        int columnAfter = (column == 0 ? 0 : (column-1));
+        int rowAfter = row;
+        if (location[columnAfter][rowAfter].hasPlanet() && columnAfter >= 0){
+            visitPlanet(sprite, column, row);}
+        else if (location[columnAfter][rowAfter].hasMeteorite() || (columnAfter < 0)) {
+            System.out.println("DEAD"); //@todo: gameover
+        } else {
+            move(sprite, location, column, row, columnAfter, rowAfter);
+        }
+    }
+    public void moveRight(Sprite sprite, int column, int row) {
+        int columnAfter = (column == 11 ? 11 : (column+1));
+        int rowAfter = row ;
+        if (location[columnAfter][rowAfter].hasPlanet() && columnAfter <12 ){
+            visitPlanet(sprite, column, row);}
+        else if (location[columnAfter][rowAfter].hasMeteorite() || (columnAfter > 11)) {
+            System.out.println("DEAD"); //@todo: gameover
+        } else {
+            move(sprite, location, column, row, columnAfter, rowAfter);
+        }
     }
 
-//    public void addObject(int amount, String string) {
-//        int i = 0;
-//        while (i < amount) {
-//            int posX = (int) Math.floor(Math.random() * 12);
-//            int posY = (int) Math.floor(Math.random() * 12);
-//
-//            if (gridPaneNodes[posX][posY] == null) {
-//                Sprite anySprite;
-//                switch (string) {
-//                    case "meteorite":
-//                        anySprite = new Meteorite();
-//                        break;
-//                    case "planet":
-//                        anySprite = new Planet();
-//                        break;
-//                    case "wormhole":
-//                        anySprite = new Wormhole();
-//                        break;
-//                    default:
-//                        //@todo handle exception maybe??
-//                        System.out.println("invalid input");
-//                        return;
-//                }
-//                gp.add(anySprite, posX, posY);
-//                gridPaneNodes[posX][posY] = anySprite;
-//                i++;
-//            }
-//        }
-//    }
+    public void visitPlanet(Sprite sprite, int column, int row){
+        move(sprite, location, column, row);
+        gp.getChildren().remove(location[column][row]);
+        ImageView image = new ImageView("planetvisited.png");
+        gp.add(image, column, row);
+        planetScore ++;
+        if (planetScore == 4){
+            addObject(location, 1, "wormhole");
+        }
+        System.out.println(planetScore);
+    }
+
+
+
+    public void addObject(Location[][] location, int amount, String string) {
+        int i = 0;
+        while (i < amount) {
+            int column = (int) Math.floor(Math.random() * 12);
+            int row = (int) Math.floor(Math.random() * 12);
+
+            if (!location[column][row].hasSprite()) {
+                Sprite anySprite;
+                switch (string) {
+                    case "meteorite":
+                        anySprite = new Meteorite();
+                        break;
+                    case "planet":
+                        anySprite = new Planet();
+                        break;
+                    case "wormhole":
+                        anySprite = new Wormhole();
+                        break;
+                    default:
+                        //@todo handle exception maybe??
+                        System.out.println("invalid input");
+                        return;
+                }
+
+                anySprite.setLocation(location[column][row]);
+                gp.add(anySprite, anySprite.getLocation().getColumn(), anySprite.getLocation().getRow());
+
+                i++;
+            }
+        }
+    }
 }
--- src/LocationService.java
+++ src/LocationService.java
@@ -6,11 +6,11 @@ public class LocationService {
     int board_size = 12;
 
     public Location[][] initalize(GridPane gp) {
-        Location[][] gridPaneNodes = new Location[board_size][board_size];
+        Location[][] location = new Location[board_size][board_size];
         //        initialize playfield
         for (int i = 0; i < board_size; i++) {
             for (int j = 0; j < board_size; j++) {
-                gridPaneNodes[i][j] = new Location(i, j);
+                location[i][j] = new Location(i, j);
                 Rectangle tile = new Rectangle(50, 50);
                 tile.setFill(Color.TRANSPARENT);
                 gp.add(tile, i, j);
@@ -19,26 +19,26 @@ public class LocationService {
 
         for (int i = 0; i < board_size; i++) {
             for (int j = 0; j < board_size; j++) {
-                gp.setColumnIndex(gridPaneNodes[i][j], i);
-                gp.setRowIndex(gridPaneNodes[i][j], j);
+                gp.setColumnIndex(location[i][j], i);
+                gp.setRowIndex(location[i][j], j);
                 if (i + 1 < board_size) {
-                    gridPaneNodes[i][j].setRight(gridPaneNodes[i + 1][j]);
+                    location[i][j].setRight(location[i + 1][j]);
                 }
 
                 if (i - 1 < board_size && i - 1 > 0) {
-                    gridPaneNodes[i][j].setLeft(gridPaneNodes[i - 1][j]);
+                    location[i][j].setLeft(location[i - 1][j]);
                 }
 
                 if (j + 1 < board_size) {
-                    gridPaneNodes[i][j].setDown(gridPaneNodes[i][j + 1]);
+                    location[i][j].setDown(location[i][j + 1]);
                 }
 
                 if (j - 1 < board_size && j - 1 > 0) {
-                    gridPaneNodes[i][j].setUp(gridPaneNodes[i][j - 1]);
+                    location[i][j].setUp(location[i][j - 1]);
                 }
             }
         }
 
-        return gridPaneNodes;
+        return location;
     }
 }
