b97c45d5000444fbcd7bef9638820e488bb178ac
darylholling <daryl_holling@hotmail.com>
2020-06-14 10:39:59 +0200

    Removed commented codelines

--- src/Galaxy.java
+++ src/Galaxy.java
@@ -1,8 +1,6 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Galaxy extends Application {
@@ -20,58 +18,11 @@ public class Galaxy extends Application {
         // add spaceship
         Spaceship spaceship = new Spaceship();
         spaceship.setLocation(gridPaneNodes[0][0]);
-        gp.add(spaceship,0,0);
-        gp.setColumnIndex(spaceship, spaceship.getLocation().getRight().getColumn());
+        gp.add(spaceship,spaceship.getLocation().getColumn(),spaceship.getLocation().getColumn());
+//        gp.setColumnIndex(spaceship, spaceship.getLocation().getRight().getColumn());
 //        gp.setRowIndex(spaceship,  gp.getRowIndex(spaceship));
         System.out.println(gp.getColumnIndex(spaceship));
 
-
-        //druk op D
-
-//        for (int i = 0; i < board_size; i++) {
-//            for (int j = 0; j < board_size; j++) {
-////                gridPaneNodes[i][j] = new Location(i, j);
-//                Rectangle tile = new Rectangle(50, 50);
-//                tile.setFill(Color.TRANSPARENT);
-////                tile.setStroke(Color.BLACK);
-//                gp.add(tile,i,j);
-//
-//            }
-//        }
-
-//
-//        for (int i = 0; i < board_size; i++) {
-//            for (int j = 0; j < board_size; j++) {
-//                if (i + 1 < board_size) {
-//                    //setright
-//                    gp.setColumnIndex(spaceship, (gp.getColumnIndex(spaceship)+1));
-//                    gp.setRowIndex(spaceship,  gp.getRowIndex(spaceship));
-//                }
-//
-//                if (i - 1 < board_size && i - 1 > 0) {
-//                    gridPaneNodes[i][j].setLeft(gridPaneNodes[i - 1][j]);
-//                }
-//
-//                if (j + 1 < board_size) {
-//                    gridPaneNodes[i][j].setDown(gridPaneNodes[i][j + 1]);
-//                }
-//
-//                if (j - 1 < board_size && j - 1 > 0) {
-//                    gridPaneNodes[i][j].setUp(gridPaneNodes[i][j - 1]);
-//                }
-//            }
-//        }
-
-//        spaceship.setLocation(gridPaneNodes[spaceship.getLocation().getRight().getX()][spaceship.getLocation().getRight().getY()]);
-//        gp.getChildren().add(spaceship);
-//        spaceship.setLocation(spaceship.getLocation().getRight());
-
-//        System.out.println(gridPaneNodes[5][5].getRight().getX());
-//        System.out.println(gridPaneNodes[5][5].getRight().getY());
-//        Spaceship spaceship = new Spaceship();
-
-//        gp.add(spaceship, 0, 0);
-//        gridPaneNodes[0][0] = space;
 //
 ////        adds 4 planets at random locations
 //        addObject(4, "planet");
@@ -83,13 +34,6 @@ public class Galaxy extends Application {
         //addObject(1, "wormhole");
 
         gp.setStyle("-fx-background-image: url('wp1.jpg');");
-
-// this function lists the content of our gridPaneNodes[][] for debugging purposes
-//        for (int i = 0; i < board_size; i++) {
-//            for (int j = 0; j < board_size; j++) {
-//                System.out.println(gridPaneNodes[i][j]);
-//            }
-//        }
 
         Scene scene = new Scene(gp, 600, 600);
         stage.setScene(scene);
