4a9d63cdb4f4f6212500e0ee830c427090507165
Mirjam <19137508@student.hhs.nl>
2020-06-17 17:18:24 +0200

    added scenes

--- src/Galaxy.java
+++ src/Galaxy.java
@@ -1,9 +1,18 @@
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuBar;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Galaxy extends Application {
@@ -11,8 +20,27 @@ public class Galaxy extends Application {
     int planetQuantity = 4;
     Location[][] location;
 
+    Scene scene1, scene2, scene3;
+
     @Override
     public void start(Stage stage) throws Exception {
+
+        stage.setTitle("Galaxy The Game!");
+
+        Button startGame = new Button("Start the game!");
+        Button highScores = new Button("View highscores");
+        Button exitGame = new Button("Quit game");
+        startGame.setOnAction(e->stage.setScene(scene2));
+//        highScores.setOnAction(e);
+        exitGame.setOnAction(e->stage.close());
+
+        VBox layout1 = new VBox(20);
+        layout1.getChildren().addAll(startGame,highScores,exitGame);
+
+        layout1.setStyle("-fx-background-image: url('Space2.png');");
+        layout1.setAlignment(Pos.CENTER);
+        scene1 = new Scene(layout1,600, 600);
+
         gp = new GridPane();
         gp.setPrefSize(600, 600);
         LocationService locationService = new LocationService();
@@ -31,11 +59,37 @@ public class Galaxy extends Application {
 
         gp.setStyle("-fx-background-image: url('wp1.jpg');");
 
-        Scene scene = new Scene(gp, 600, 600);
-        stage.setScene(scene);
+        Button button3 = new Button("Easy");
+        button3.setOnAction(e->stage.setScene(scene3));
+        Button button4 = new Button("Hard");
+//        button4.setOnAction(e->stage.setScene(scene2));
+        Button button5 = new Button("Very Hard");
+//    button1.setOnAction(e->stage.setScene(scene2));
+        Button button6 = new Button("Go back");
+        button6.setOnAction(e->stage.setScene(scene1));
+//
+        button3.setMaxWidth(120);
+        button4.setMaxWidth(120);
+        button5.setMaxWidth(120);
+        button6.setMaxWidth(120);
+
+        VBox layout2 = new VBox(20);
+        layout2.setAlignment(Pos.CENTER);
+
+
+
+
+        layout2.getChildren().addAll(button3, button4, button5, button6);
+
+        scene2 = new Scene(layout2, 600, 600);
+        layout2.setStyle("-fx-background-image: url('galaxy-menu.png');");
+        scene3 = new Scene(gp, 600, 600);
+        stage.setScene(scene1);
         stage.show();
+
+
         //move spaceship around with arrows
-        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+        scene3.setOnKeyPressed(new EventHandler<KeyEvent>() {
             @Override
             public void handle(KeyEvent keyEvent) {
                 switch (keyEvent.getCode()) {
