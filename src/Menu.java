fa0d67c852d38e791430e1bba03bfec1feab5121
Mirjam <19137508@student.hhs.nl>
2020-06-24 19:30:22 +0200

    merge master

--- src/Menu.java
+++ src/Menu.java
@@ -3,6 +3,7 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -11,8 +12,6 @@ import javafx.stage.Stage;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import java.io.File;
-
-
 
 public class Menu {
     Scene scene1, scene2;
@@ -23,14 +22,11 @@ public class Menu {
     public void initalize(Stage stage, GridPane gridPane, Scene gameScene, Timer timer, SpriteService spriteService, Game game) throws Exception {
         this.spriteService = spriteService;
         this.game = game;
-//        Scanner keyboard = new Scanner(System.in);
-//        int answer = Integer.parseInt(keyboard.nextLine());
-//        int answer2 = Integer.parseInt(keyboard.nextLine());
 
         String musicFile = "src\\Star Wars.mp3";
         Media sound = new Media(new File(musicFile).toURI().toString());
         mediaPlayer = new MediaPlayer(sound);
-//      mediaPlayer.play();
+        mediaPlayer.play();
 
         stage.setTitle("Galaxy Quest!");
         Label welcome = new Label("Galaxy Quest!");
@@ -76,16 +72,6 @@ public class Menu {
             stage.setScene(gameScene);
             timer.start();
         });
-//       Button custom = new Button("Custom");
-//        custom.setOnAction(e -> stage.setScene(scene3));
-
-//
-//        custom.setOnAction(e-> {
-//
-//            this.startGame(answer, answer2, stage, gameScene);
-//            stage.setScene(gameScene);
-//            timer.start();
-//        });
 
         Button goBack = new Button("Go back");
         goBack.setOnAction(e -> stage.setScene(scene1));
@@ -100,36 +86,7 @@ public class Menu {
         layoutChooseLevel.setStyle("-fx-background-image: url('galaxy-menu.png');");
         scene2 = new Scene(layoutChooseLevel, 600, 600);
 
-//        VBox layoutChooseSprites = new VBox(20);
-//
-//        Label choosePlanets = new Label("Choose the amount of planets: ");
-//        Label chooseMeteorites = new Label("Choose the amount of planets: ");
-//        TextField amountPlanets = new TextField();
-//        amountPlanets.setMaxWidth(50);
-//        TextField amountMeteorites = new TextField();
-//        amountMeteorites.setMaxWidth(50);
-//        Button startCustom = new Button("Start the game");
-//        startCustom.setOnAction(e-> {
-//            this.startGame(amountMeteorites.getText(), amountPlanets.getText(), stage, gameScene);
-//            stage.setScene(gameScene);
-//            timer.start();
-//        });
-//
-//
-//        choosePlanets.setFont(Font.font("Verdana", 30));
-//        choosePlanets.setTextFill(Color.WHITE);
-//        chooseMeteorites.setFont(Font.font("Verdana", 30));
-//        chooseMeteorites.setTextFill(Color.WHITE);
-//
-//       layoutChooseSprites.setAlignment(Pos.CENTER);
-//       layoutChooseSprites.getChildren().addAll(choosePlanets,amountPlanets, chooseMeteorites, amountMeteorites, startCustom, goBack);
-//       layoutChooseSprites.setStyle("-fx-background-image: url('galaxy-menu.png');");
-//        scene3 = new Scene(layoutChooseSprites, 600, 600);
-
-
-
-
-                stage.setScene(scene1);
+        stage.setScene(scene1);
         stage.show();
     }
 
